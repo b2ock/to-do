47f234750fece2ac4aece1f1fd7e83b3f211bd6b
b2ock <brockwood4@gmail.com>
2023-03-29 15:03:50 -0600

    Reset for the start of the project

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 "use strict"
 
-function inputButton(id) {
-    
-    let parent = document.getElementById(id+'-list');
-    let child = document.createElement('li');
-    child.innerText = document.getElementById(id+'-input').value;
-
-    parent.appendChild(child);
-}
-
