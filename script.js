d7f6345bdd4cf1fd216374612d1b19ad5b631d37
b2ock <brockwood4@gmail.com>
2023-04-05 14:19:10 -0600

    Update
    Created three lists, still need to make additional items add to the right list and changing colors. Need to also think about how to make items as objects so they can hold more data

--- script.js
+++ script.js
@@ -2,12 +2,36 @@
 
 let body = document.getElementById('body');
 let itemTemplate = document.getElementById('list-items');
+const lists = [];
+const listOne = [];
+
+createLists();
+createLists();
 
 function createNewTask() {
     let newItem = itemTemplate.children[0].cloneNode(true);
     let addItem = document.getElementById('add-list-item');
 
+    newItem.style.display = '';
+
     itemTemplate.appendChild(newItem);
 
     itemTemplate.appendChild(addItem);
+}
+
+function deleteItem(item) {
+
+    item.parentNode.parentNode.remove();
+}
+
+function displayItems(list, array) {
+
+}
+
+function createLists(color, id) {
+
+    let lists = document.getElementById('lists');
+    let newList = lists.children[0].cloneNode(true);
+
+    lists.appendChild(newList);
 }
