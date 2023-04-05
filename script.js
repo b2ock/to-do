ded721a4ac6cbb48b7bbd059b4b5680f79e44196
b2ock <brockwood4@gmail.com>
2023-04-04 18:43:07 -0600

    Rework
    Updated pretty much everything again. Have some basics working but not complete

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 "use strict"
 
+let body = document.getElementById('body');
+let itemTemplate = document.getElementById('list-items');
+
+function createNewTask() {
+    let newItem = itemTemplate.children[0].cloneNode(true);
+    let addItem = document.getElementById('add-list-item');
+
+    itemTemplate.appendChild(newItem);
+
+    itemTemplate.appendChild(addItem);
+}
